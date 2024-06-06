7b7894a5e734b4d1bd8641d8efcdb88b58d508f9
kaosar ahamed <kaocar07@gmail.com>
2024-06-06 21:55:49 +0600

    commited

--- src/dashboard/components/MobileMenu.jsx
+++ src/dashboard/components/MobileMenu.jsx
@@ -1,86 +1,71 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Style from "../styles/Mobilemenu.module.css";
-import PropTypes from 'prop-types';
-import { FaTable, FaThLarge, FaCloudUploadAlt, FaCog, FaUserPlus, FaBoxOpen, FaExternalLinkAlt} from "react-icons/fa";
+import PropTypes from "prop-types";
+import {
+  FaThLarge,
+  FaUserCircle,
+  FaCog,
+  FaRegCalendarPlus,
+  FaUserFriends,
+  FaEnvelope,
+  FaCloudUploadAlt,
+  FaClipboardList,
+} from "react-icons/fa";
 function MobileMenu(props) {
-    
-    const warehouseUser = localStorage.getItem("warehouseusername");
-    const csruser = localStorage.getItem("csrusername");
-
-  const navigate = useNavigate()
-  const logut = () => {
-    localStorage.removeItem("warehouseusername");
-        localStorage.removeItem("warehousetoken");
-        localStorage.removeItem("warehouseId");
-        localStorage.removeItem("csrusername");
-        localStorage.removeItem("csrtoken");
-        localStorage.removeItem("csrId");
-    navigate("/login")
-    props.setMenu(!props.menu)
-  }
-  const warehouselogout = () => {
-    localStorage.removeItem("warehouseusername");
-    localStorage.removeItem("warehousetoken");
-    localStorage.removeItem("warehouseId");
-    localStorage.removeItem("csrusername");
-    localStorage.removeItem("csrtoken");
-    localStorage.removeItem("csrId");
-    navigate("/warehouseregistration")
-}
   return (
-    <div className={`${Style.Mobilemenu} ${
-      props.menu ? `${Style.active}` : `${Style.inactive}`
-    }`} ref={props.menuref}>
-        <div className={Style.menuNav}>
-          <ul>
-          {warehouseUser && <li onClick={() => props.setMenu(!props.menu)}>
-                        <FaThLarge />
-                        <Link to="/dashboard/warehouse">Warehouse</Link>
-                    </li>}
+    <div
+      className={`${Style.Mobilemenu} ${
+        props.menu ? `${Style.active}` : `${Style.inactive}`
+      }`}
+      ref={props.menuref}
+    >
+      <div className={Style.menuNav}>
+        <ul>
+          <li>
+            <FaThLarge />
+            <Link to="/dashboard/events">Events</Link>
+          </li>
 
-                    <li onClick={() => props.setMenu(!props.menu)}>
-                        <FaBoxOpen />
-                        <Link to="/dashboard/packages">Packages</Link>
-                    </li>
-                    <li onClick={() => props.setMenu(!props.menu)}>
-                        <FaTable />
-                        <Link to="/dashboard/csr">CSR</Link>
-                    </li>
-                    {warehouseUser && <li onClick={() => props.setMenu(!props.menu)}>
-                        <FaCloudUploadAlt />
-                        <Link to="/dashboard/upload">Upload Products</Link>
-                    </li>}
-                    {warehouseUser && <li onClick={() => props.setMenu(!props.menu)}>
-                        <FaCog />
-                        <Link to="/dashboard/warehousesetting">Setting</Link>
-                      </li>}
-                        {csruser && 
-                        <li onClick={() => props.setMenu(!props.menu)}>
-                        <FaCog />
-                        <Link to="/dashboard/csrsetting">Setting</Link>
-                        </li>}
-                        <li>
-                        <FaUserPlus />
-                        <Link onClick={warehouselogout}>Create User</Link>
-                        </li>
-                    {warehouseUser ? <li onClick={logut}>
-                      
-                      <FaExternalLinkAlt />
-                      <Link to="">Logout</Link></li> 
-                       : csruser ? <li onClick={logut}><FaExternalLinkAlt /><Link to="">Logout</Link></li> : null }
-                    
-              
-          </ul>
-        </div>
+          <li>
+            <FaRegCalendarPlus />
+            <Link to="/dashboard/create">Create Event</Link>
+          </li>
+          <li>
+            <FaCloudUploadAlt />
+            <Link to="/dashboard/submissions">Submissions</Link>
+          </li>
+          <li>
+            <FaClipboardList />
+            <Link to="/dashboard/brands">Brands</Link>
+          </li>
+          <li>
+            <FaUserFriends />
+            <Link to="/dashboard/users">Users</Link>
+          </li>
+          <li>
+            <FaEnvelope />
+            <Link to="/dashboard/emails">Emails</Link>
+          </li>
+
+          <li>
+            <FaUserCircle />
+            <Link to="/dashboard/profile">Profile</Link>
+          </li>
+
+          <li>
+            <FaCog />
+            <Link to="/dashboard/setting">Setting</Link>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
 MobileMenu.propTypes = {
-    menu: PropTypes.any,
-    menuref : PropTypes.any,
-    setMenu : PropTypes.any
-  }
-
+  menu: PropTypes.any,
+  menuref: PropTypes.any,
+  setMenu: PropTypes.any,
+};
 
 export default MobileMenu;
